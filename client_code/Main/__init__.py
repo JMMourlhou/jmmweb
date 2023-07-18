40ac0e40f59a3e3d2738e687e69c307b66e9b888
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-07-18 20:17:47 +0000

    Edited Main and theme/parameters.yaml

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -16,14 +16,16 @@ class Main(MainTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.content_panel.add_component(Home(), full_width_row=True)
-    bt = Label(align="center", bold=False, foreground="white", background="b", text="Essai", role="raised", tag="", )
+      
+    bt = Label(align="center", text="Essai", bold=False, foreground="red", background="#FF000000", role="raised", tag="" )
     self.column_panel_1.add_component(bt)
     bt.enabled = True
-    
+    bt.role = "raised"
+    bt.raise_event()
+      
   def contact_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.content_panel.clear()
-     
     self.content_panel.add_component(Contact(), full_width_row=True)
 
   def home_link_click(self, **event_args):
