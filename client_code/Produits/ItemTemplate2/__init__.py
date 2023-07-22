8827a5fa2d29b1643bc821ef7b4698e94097a7ea
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-07-22 15:12:29 +0000

    Edited Main, Produits.ItemTemplate2 and Produits

--- client_code/Produits/ItemTemplate2/__init__.py
+++ client_code/Produits/ItemTemplate2/__init__.py
@@ -11,6 +11,6 @@ class ItemTemplate2(ItemTemplate2Template):
         self.init_components(**properties)
         
         # Any code you write here will run before the form opens.
-        self.label_1.text = self.item['prestation']
-        self.label_2.text = self.item['tarif-1-jour']
-        self.label_3.text = self.item['tarif-1/2-jour']
+        self.text_box_1.text = self.item['prestation']
+        self.text_box_2.text = self.item['tarif-1-jour']
+        self.text_box_3.text = self.item['tarif-1/2-jour']
