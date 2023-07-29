01e3e0505dbcac518c66de510bb85daf6faccea8
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-07-29 16:28:39 +0000

    Edited Produits

--- client_code/Produits/__init__.py
+++ client_code/Produits/__init__.py
@@ -5,22 +5,19 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-global liste_prestation
-liste_prestation = app_tables.produits.search()
-
 
 class Produits(ProduitsTemplate):
     def __init__(self, condition="", **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        liste_prestations = app_tables.produits.search()
-        for produit in liste_prestations:
-            if produit["code"]==1:
-                self.button_1.text = produit['prestation']
+        liste_prestation = app_tables.produits.search()
+        for row in liste_prestations:
+            if row["code"]==1:
+                self.button_1.text = row['prestation']
             if produit["code"]==2:
-                self.button_2.text = produit['prestation']
+                self.button_2.text = row['prestation']
             if produit["code"]==3:
-                self.button_3.text = produit['prestation']
+                self.button_3.text = row['prestation']
             
         # selects the prestations starting with "SST"
         # condition="SST%"
@@ -30,13 +27,9 @@ class Produits(ProduitsTemplate):
 
     
     def affiche_prix(self, condition):
-        global liste_prestation
-        for row in liste_prestation:
-            print(row["prestation"], row["tarif-1-jour"])
-        # selects the prestations starting with "SST"
+        # selects the prestations starting with var condition
         # ex condition="SST%"
-        #self.repeating_panel_1.items = app_tables.produits.search(prestation=q.ilike(condition))
-        self.repeating_panel_1.items = liste_prestation.search(prestation=q.ilike(condition))
+        self.repeating_panel_1.items = app_tables.produits.search(prestation=q.ilike(condition))
         self.data_grid_1.visible = True
         
     def code1_click(self, **event_args):
