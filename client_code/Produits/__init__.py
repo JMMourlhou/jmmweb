50dc3d6236e63be6ce419fec89ea71c2cee084e8
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-07-29 10:46:56 +0000

    Created Form1; Edited Produits

--- client_code/Produits/__init__.py
+++ client_code/Produits/__init__.py
@@ -8,10 +8,12 @@ from anvil.tables import app_tables
 
 
 class Produits(ProduitsTemplate):
-    def __init__(self, **properties):
+    def __init__(self, condition="", **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        self.repeating_panel_1.items = app_tables.produits.search()
+        # selects the prestations starting with "SST"
+        condition="SST%"
+        self.repeating_panel_1.items = app_tables.produits.search(prestation=q.ilike(condition))
         
         # Any code you write here will run when the form opens.
         
