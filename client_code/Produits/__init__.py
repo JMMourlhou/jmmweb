c7d706fedcd3b094d4a0c6a437cb6554d75ff90a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-07-29 12:37:59 +0000

    Created etude risques.jpg; Edited settings and Produits; Deleted Form1

--- client_code/Produits/__init__.py
+++ client_code/Produits/__init__.py
@@ -11,9 +11,39 @@ class Produits(ProduitsTemplate):
     def __init__(self, condition="", **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        liste_prestations = app_tables.produits.search()
+        for produit in liste_prestations:
+            if produit["code"]==1:
+                self.button_1.text = produit['prestation']
+            if produit["code"]==2:
+                self.button_2.text = produit['prestation']
+            if produit["code"]==3:
+                self.button_3.text = produit['prestation']
+            
         # selects the prestations starting with "SST"
         condition="SST%"
         self.repeating_panel_1.items = app_tables.produits.search(prestation=q.ilike(condition))
         
         # Any code you write here will run when the form opens.
+
+    
+    def affiche_prix(self, condition):
+        # selects the prestations starting with "SST"
+        # ex condition="SST%"
+        self.repeating_panel_1.items = app_tables.produits.search(prestation=q.ilike(condition))
+    
+    def code1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.affiche_prix("SST-I%")
+
+    def code2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.affiche_prix("SST-F%")
+
+    def code3_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.affiche_prix("PSC%")
+
+
+
         
