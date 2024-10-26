7473d7254e78474b973309a712fbd7d91c096b31
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-26 13:24:20 +0000

    Edited french_zone

--- client_code/french_zone.py
+++ client_code/french_zone.py
@@ -1,13 +1,15 @@
 import anvil.server
 # Pour le calcul de l'heure en France
 from anvil import *  #pour les alertes
-import anvil.tz
 from datetime import datetime
-import pytz
+from zoneinfo import ZoneInfo
+
+
 
 #Get the time now, local time.
 def french_zone_time():
     #date_time = datetime.now(anvil.tz.tzlocal()) #recup browser time
-    tz = pytz.timezone('Europe/Paris')
-    date_time = datetime.now(tz=tz)
+    #tz = pytz.timezone('Europe/Paris')
+    dt = datetime.now(ZoneInfo("Europe/Paris"))
+    #date_time = datetime.now(tz=tz)
     return date_time
