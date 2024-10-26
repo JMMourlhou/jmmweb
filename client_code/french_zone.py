ed4ecdb8f84655de476ad5bd3d121729249e19ec
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-26 13:30:46 +0000

    Created custom package dependencies; Edited settings and french_zone

--- client_code/french_zone.py
+++ client_code/french_zone.py
@@ -2,14 +2,13 @@ import anvil.server
 # Pour le calcul de l'heure en France
 from anvil import *  #pour les alertes
 from datetime import datetime
-from zoneinfo import ZoneInfo
+import anvil.tz
 
 
 
 #Get the time now, local time.
 def french_zone_time():
-    #date_time = datetime.now(anvil.tz.tzlocal()) #recup browser time
-    #tz = pytz.timezone('Europe/Paris')
-    dt = datetime.now(ZoneInfo("Europe/Paris"))
-    #date_time = datetime.now(tz=tz)
+    date_time = datetime.now(anvil.tz.tzlocal()) #recup browser time
+    print(type(date_time))
+    print(date_time)
     return date_time
