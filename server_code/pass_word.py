2b8cd596554d9cbced7462518d66d4b39ebfde2a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-12-29 19:48:19 +0000

    Edited pass_word

--- server_code/pass_word.py
+++ server_code/pass_word.py
@@ -4,6 +4,7 @@ import anvil.server
 #Test du Mot de passe par test du pw enregistré en secret
 @anvil.server.callable
 def pw(pw):
+    if pw == anvil.secrets.get_secret("pass_word"):
         result = True
     else:
         result = False
